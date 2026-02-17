1f3ce3369628239e58db99c1e849cfb84c174b5a
Mikesilver19 <mikevideos45@gmail.com>
2026-02-17 18:29:52 -0500

    Add files via upload

--- base-LSE.py
+++ base-LSE.py
@@ -30,3 +30,99 @@ class ListaSE:
 		else:
 			nuevo_nodo.siguiente = self.cabeza
 			self.cabeza = nuevo_nodo
+
+	def agregarFinal(self, data):
+		nuevo_nodo = Nodo(data)
+		if self.cabeza is None:
+			self.cabeza = nuevo_nodo
+			return
+		actual = self.cabeza
+		while actual.siguiente:
+			actual = actual.siguiente
+		actual.siguiente = nuevo_nodo
+	
+	def insertarAntesDe(self, X, data):
+		actual = self.cabeza
+		while actual is not None:
+			if actual.data == X:
+				actual=Nodo(data)
+				actual.siguiente = X
+				X=X.siguiente
+				return
+		
+	def insertarAntes(self, X, data):
+		if self.cabeza is None:
+			print("Lista vacía")
+			return
+
+		if self.cabeza.data == X:
+			nuevo = Nodo(data)
+			nuevo.siguiente = self.cabeza
+			self.cabeza = nuevo
+			return
+
+		actual = self.cabeza
+		while actual.siguiente and actual.siguiente.data != X:
+			actual = actual.siguiente
+
+		if actual.siguiente is None:
+			print("Elemento no encontrado")
+		else:
+			nuevo = Nodo(data)
+			nuevo.siguiente = actual.siguiente
+			actual.siguiente = nuevo
+
+	def insertarDespues(self, X, data):
+		actual = self.cabeza
+
+		while actual and actual.data != X:
+			actual = actual.siguiente
+
+		if actual is None:
+			print("Elemento no encontrado")
+		else:
+			nuevo = Nodo(data)
+			nuevo.siguiente = actual.siguiente
+			actual.siguiente = nuevo
+
+	def eliminarPrimero(self):
+		if self.cabeza is None:
+			print("Lista vacía")
+		else:
+			self.cabeza = self.cabeza.siguiente
+
+	def eliminarUltimo(self):
+		if self.cabeza is None:
+			print("Lista vacía")
+			return
+
+		if self.cabeza.siguiente is None:
+			self.cabeza = None
+			return
+
+		actual = self.cabeza
+		while actual.siguiente.siguiente:
+			actual = actual.siguiente
+
+		actual.siguiente = None
+
+	def buscar(self, valor):
+		actual = self.cabeza
+
+		while actual:
+			if actual.data == valor:
+				return True
+			actual = actual.siguiente
+
+		return False
+	
+	def contar(self):
+		contador = 0
+		actual = self.cabeza
+
+		while actual:
+			contador += 1
+			actual = actual.siguiente
+
+		return contador
+
